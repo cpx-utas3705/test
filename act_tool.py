b6d54a755b8756bc7b0b0718aa0e5ffdf5b367fe
cpx-utas3705 <chenps.utas.edu@gmail.com>
2026-08-30 18:06:21 +1000

    yess

--- act_tool.py
+++ act_tool.py
@@ -1,3 +1,13 @@
+from dataclasses import dataclass
+
+@dataclass
+class acts_data:
+    name: str
+    stage: str 
+    category: str
+    minutes: int
+    confirmed: bool
+
 acts = [
     {"name": "The Midnight Tacos", "stage": "Main", "category": "music", "minutes": 45, "confirmed": True},
     {"name": "Laser Koala", "stage": "Garden", "category": "dance", "minutes": 30, "confirmed": False},
@@ -5,10 +15,30 @@ acts = [
     {"name": "Jokes About Bread", "stage": "Main", "category": "comedy", "minutes": 25, "confirmed": False}
 ]
 
-
+#display the acts in a formatted table
 def acts_format(acts):
-formatted_acts = []
+    print("\n")
     for act in acts:
-        formatted_act = f"{act['name']} - {act['stage']} - {act['category']} - {act['minutes']} minutes - {'Confirmed' if act['confirmed'] else 'Not Confirmed'}"
-        formatted_acts.append(formatted_act)
-   print(formatted_acts) 
+        print (f"{act['name']:<20} {act['stage']:<10} {act['category']:<10} {act['minutes']:<5} {act['confirmed']}")
+    print("\n")
+
+#add new act to list
+def add_act(name,stage,category,minutes,confirmed):
+    acts.append({"name":name,"stage":stage,"category":category,"minutes":minutes,"confirmed":confirmed})
+
+#Mark act as confirmed
+def confirm_act(confirm_name):
+    for act in acts:
+        if act['name']==confirm_name:
+            act['confirmed'] = True
+            print(f"{confirm_name} has been confirmed.")
+            return
+        else:
+            print("Act not found")
+
+#view acts by stage
+def view_acts_by_stage(stage):
+    for act in acts:
+        if act['stage']== stage:
+            print(f"{act['name']:<20} {act['category']:<10}{act['minutes']:<5} {act['confirmed']:<5}")
+
